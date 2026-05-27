86e23f5844806d3c04d67ca8ec7fab52334c43f3
jujulioed <julioeduardon9@hotmail.com>
2026-05-26 23:37:47 -0300

    Implementing an array - add to the end function

--- src/main/java/com/jujulioed/javadsa/vetor/TesteVetor.java
+++ src/main/java/com/jujulioed/javadsa/vetor/TesteVetor.java
@@ -2,7 +2,11 @@ package com.jujulioed.javadsa.vetor;
 
 public class TesteVetor {
     public static void main(String[] args) { 
-        Vetor vetor = new Vetor(20);
+        Vetor vetor = new Vetor(2);
+
+        vetor.adiciona("Elemento 1");
+        vetor.adiciona("Elemento 2");
+        vetor.adiciona("Elemento 3");
 
         int i = 0;
     }
--- src/main/java/com/jujulioed/javadsa/vetor/Vetor.java
+++ src/main/java/com/jujulioed/javadsa/vetor/Vetor.java
@@ -3,9 +3,30 @@ package com.jujulioed.javadsa.vetor;
 public class Vetor {
 
     private String[] elementos;
+    private int tamanho;
 
     public Vetor(int capacidade) {
         this.elementos = new String[capacidade];
+        this.tamanho = 0;
+    }
+
+    // Jeito 1: não otimizado
+    // public void adiciona(String elemento) {
+    //     for (int i = 0; i < this.elementos.length; i++) {
+    //         if (this.elementos[i] == null) {
+    //             this.elementos[i] = elemento;
+    //             break;
+    //         }
+    //     }
+    // }
+
+    public boolean adiciona(String elemento) {
+        if (tamanho < elementos.length) {
+            elementos[tamanho] = elemento;
+            this.tamanho++;
+            return true;
+        }
+        return false;
     }
 
 }
