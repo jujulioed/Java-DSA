cb798912ccb005ce74a798cd165133cdaad499b5
jujulioed <julioeduardon9@hotmail.com>
2026-05-26 23:16:52 -0300

    Creating an implementation of array

---
+++ src/main/java/com/jujulioed/javadsa/vetor/TesteVetor.java
@@ -0,0 +1,9 @@
+package com.jujulioed.javadsa.vetor;
+
+public class TesteVetor {
+    public static void main(String[] args) { 
+        Vetor vetor = new Vetor(20);
+
+        int i = 0;
+    }
+}
---
+++ src/main/java/com/jujulioed/javadsa/vetor/Vetor.java
@@ -0,0 +1,11 @@
+package com.jujulioed.javadsa.vetor;
+
+public class Vetor {
+
+    private String[] elementos;
+
+    public Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+    }
+
+}
